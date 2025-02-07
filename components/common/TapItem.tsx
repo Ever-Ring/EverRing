ee15a0efdc0315943070df4e119fe266fe5c9296
yejiniee <02doyoung01@naver.com>
2025-02-08 00:39:34 +0900

    Revert "🎨 : Tab 애니메이션 추가"
    This reverts commit 84c99323093bd0ef7de1eb693e35fd135d07946e.

--- components/common/TapItem.tsx
+++ components/common/TapItem.tsx
@@ -1,9 +1,7 @@
-/* eslint-disable react/require-default-props */
 // TODO 나중에 구름/나무로 이미지 바꿀 것. 구름링/나무링
 
-import SvgCloud from "@assets/dalaemfit.svg";
-import SvgTree from "@assets/workation.svg";
-import { forwardRef } from "react";
+import SvgCloud from "@image/dalaemfit.svg";
+import SvgTree from "@image/workation.svg";
 
 interface TapProps {
   title: string;
@@ -12,51 +10,46 @@ interface TapProps {
   onClick: () => void;
 }
 
-const TapItem = forwardRef<HTMLButtonElement, TapProps>(
-  ({ title, hasIcon = false, isSelected = false, onClick }, ref) => {
-    return (
-      <button
-        onClick={onClick}
-        type="button"
-        ref={ref}
-        className="flex flex-col gap-1 w-fit items-start transition-colors duration-300 ease-in-out "
-      >
-        <div className="flex flex-row gap-1 items-center">
-          <div
-            className={`font-semibold text-lg transition-colors duration-300 ease-in-out ${isSelected ? "text-gray-900" : "text-gray-400"}`}
-          >
-            {title}
-          </div>
-          {hasIcon &&
-            (title === "구름링" ? (
-              <SvgCloud
-                width={32}
-                height={32}
-                className={`transition-colors duration-300 ease-in-out ${
-                  isSelected ? "fill-gray-900" : "fill-gray-400"
-                }`}
-              />
-            ) : (
-              <SvgTree
-                width={32}
-                height={32}
-                className={`transition-colors duration-300 ease-in-out ${
-                  isSelected ? "fill-gray-900" : "fill-gray-400"
-                }`}
-              />
-            ))}
-        </div>
-      </button>
-    );
-  },
-);
-// displayName 추가 (린트 에러)
-TapItem.displayName = "TapItem";
+export default function TapItem({
+  title,
+  hasIcon,
+  isSelected,
+  onClick,
+}: TapProps) {
+  return (
+    <button
+      onClick={onClick}
+      type="button"
+      className="flex flex-col gap-1 w-fit items-start"
+    >
+      <div className="flex flex-row gap-1 items-center">
+        <div className="font-semibold text-lg text-gray-900">{title}</div>
+        {hasIcon &&
+          (title === "구름링" ? (
+            <SvgCloud
+              width={32}
+              height={32}
+              className="transition-all transform hover:scale-110"
+            />
+          ) : (
+            <SvgTree
+              width={32}
+              height={32}
+              className="transition-all transform hover:scale-110"
+            />
+          ))}
+      </div>
+
+      {/* 클릭된 상태면 visivle */}
+      <div
+        className={`bg-gray-900 w-full h-[2px] ${isSelected ? "" : "invisible"}`}
+      />
+    </button>
+  );
+}
 
 // defaultProps를 사용하여 기본값을 설정
-// TapItem.defaultProps = {
-//   hasIcon: false,
-//   isSelected: false,
-// };
-
-export default TapItem;
+TapItem.defaultProps = {
+  hasIcon: false,
+  isSelected: false,
+};
--- components/common/TapList.tsx
+++ components/common/TapList.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useRef, useEffect } from "react";
+import { useState } from "react";
 import TapItem from "@components/common/TapItem";
 
 interface TabListProps {
@@ -9,43 +9,23 @@ interface TabListProps {
 }
 
 export default function TapList({ tabs, hasIcon }: TabListProps) {
-  const [selectedIndex, setSelectedIndex] = useState<number>(0);
-  const [tabWidths, setTabWidths] = useState<number[]>([]);
-  const tabRefs = useRef<(HTMLButtonElement | null)[]>([]);
-
-  useEffect(() => {
-    const widths = tabRefs.current.map((tab) => tab?.offsetWidth || 0);
-    setTabWidths(widths);
-  }, [tabs]);
+  const [selectedIndex, setSelectedIndex] = useState<number>(0); // 기본적으로 첫 번째 탭이 선택되도록 설정
 
   const handleTabItemClick = (index: number) => {
     setSelectedIndex(index);
   };
-
   return (
-    <div className="flex flex-col items-start">
-      <div className="flex flex-row items-start gap-3">
-        {tabs.map((tab, index) => (
-          <TapItem
-            ref={(el) => {
-              tabRefs.current[index] = el;
-            }}
-            // eslint-disable-next-line react/no-array-index-key
-            key={index}
-            title={tab}
-            hasIcon={hasIcon}
-            isSelected={selectedIndex === index}
-            onClick={() => handleTabItemClick(index)}
-          />
-        ))}
-      </div>
-      <div
-        className="h-[2px] rounded-md bg-black transition-all duration-300 ease-in-out"
-        style={{
-          width: `${tabWidths[selectedIndex]}px`,
-          transform: `translateX(${tabWidths.slice(0, selectedIndex).reduce((acc, width) => acc + width, 0) + selectedIndex * 12}px)`, // (이전 탭들의 너비+gap)만큼 이동
-        }}
-      />
+    <div className="flex flex-row items-start gap-3">
+      {tabs.map((tab, index) => (
+        <TapItem
+          // eslint-disable-next-line react/no-array-index-key
+          key={index}
+          title={tab}
+          hasIcon={hasIcon}
+          isSelected={selectedIndex === index}
+          onClick={() => handleTabItemClick(index)}
+        />
+      ))}
     </div>
   );
 }
