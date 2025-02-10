9cd06de608884009ee4906a5a46dbe8811f4bd3f
giseon <pakox123@gmail.com>
2025-02-10 11:40:52 +0900

    ✨ feat: 컴포넌트 기능추가

--- components/common/Chip.tsx
+++ components/common/Chip.tsx
@@ -1,7 +1,18 @@
 interface ChipProps {
   label: string;
+  selected?: boolean;
+  onClick?: () => void;
 }
 
-export default function Chip({ label }: ChipProps) {
-  return <button>{label}</button>;
+export default function Chip({ label, selected = false, onClick }: ChipProps) {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      aria-pressed={selected}
+      title={label}
+    >
+      {label}
+    </button>
+  );
 }
