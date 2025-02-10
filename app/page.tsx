f6b0969e8bfd0f636420003653db6d5814d7e600
imsoohyeok <skilldoy2@naver.com>
2025-02-10 14:46:57 +0900

    � test: button 컴포넌트 확인용 코드 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,3 @@
-import Button from "@components/common/button";
-
 export default function Home() {
-  return (
-    <div>
-      initial commit
-      <Button text="생성하기" size="small" variant="outlined" />
-    </div>
-  );
+  return <div>initial commit</div>;
 }
