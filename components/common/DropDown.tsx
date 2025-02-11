c9e6cc2c55ff9918cd96930b68f6efcb59f81e96
imsoohyeok <skilldoy2@naver.com>
2025-02-11 18:07:36 +0900

    style: arrow-icon 종류 추가

--- components/common/DropDown.tsx
+++ components/common/DropDown.tsx
@@ -1,18 +1,26 @@
 /* eslint-disable react/require-default-props */
 import React, { useState } from "react";
 import DropDownList from "@components/common/DropDownList";
-import ArrowIcon from "@assets/icon-arrow-default-down.svg";
+import ArrowIconDefault from "@assets/icon-arrow-default-down.svg";
+import ArrowIconInverse from "@assets/icon-arrow-inverse-down.svg";
 
 const fixedItemsList = ["을지로 3가", "홍대입구", "강남역", "건대입구"];
 
 interface DropDownProps {
   onSelect: (item: string) => void;
   size?: "large" | "small";
+  iconType?: "default" | "inverse";
 }
 
-function DropDown({ onSelect, size = "large" }: DropDownProps) {
+function DropDown({
+  onSelect,
+  size = "large",
+  iconType = "default",
+}: DropDownProps) {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedItem, setSelectedItem] = useState<string | null>(null);
+  const ArrowIcon =
+    iconType === "default" ? ArrowIconDefault : ArrowIconInverse;
 
   return (
     <div className="relative">
