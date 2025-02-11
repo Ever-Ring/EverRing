5b764427f1ea078bfb6f640bba998242814c41f4
giseon <pakox123@gmail.com>
2025-02-11 16:15:49 +0900

    ✨ feat : SortFilter 생성

---
+++ components/common/SortFilter.tsx
@@ -0,0 +1,21 @@
+import SortDefault from "@assets/icon-sort-default.svg";
+
+interface SortFilterProps {
+  label: string;
+  onClick: () => void;
+}
+
+export default function SortFilter({ label, onClick }: SortFilterProps) {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      aria-label={label}
+      className="inline-flex h-9 w-9 items-center justify-start gap-1 rounded-lg bg-white p-[6px] text-sm text-gray-900 transition active:scale-95 sm:h-10 sm:w-[120px] sm:px-3 sm:py-2"
+    >
+      <SortDefault />
+
+      <div className="hidden sm:block">{label}</div>
+    </button>
+  );
+}
