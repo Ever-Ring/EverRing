ba6740be5b13d134451da848163fef28baa4b661
minzoo <aw12813@gmail.com>
2025-02-12 18:23:06 +0900

    ✨ feat: StateChip 컴포넌트 변수명 수정

--- features/StateChip.tsx
+++ features/StateChip.tsx
@@ -1,21 +1,23 @@
 import Image from "next/image";
 
 interface StateChipProps {
-  completed?: boolean;
+  isCompleted?: boolean;
   isPending?: boolean;
 }
 
-export default function StateChip({ completed, isPending }: StateChipProps) {
+export default function StateChip({ isCompleted, isPending }: StateChipProps) {
   return (
     <div className="flex flex-row gap-x-2">
       <div
         className={`box-border flex items-center rounded-full border-2 border-transparent px-3 py-1 ${
-          completed ? "bg-gray-200 text-gray-500" : "bg-mint-100 text-mint-600"
+          isCompleted
+            ? "bg-gray-200 text-gray-500"
+            : "bg-mint-100 text-mint-600"
         }`}
       >
-        <p>{completed ? "이용 완료" : "이용 예정"}</p>
+        <p>{isCompleted ? "이용 완료" : "이용 예정"}</p>
       </div>
-      {!completed && (
+      {!isCompleted && (
         <div
           className={`box-border rounded-full px-3 py-1 ${
             isPending
