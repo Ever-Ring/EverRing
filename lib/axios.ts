6370cb2423834e121bc6738c03a7f174d962c587
yejiniee <02doyoung01@naver.com>
2025-02-12 14:38:33 +0900

    ✨ feat: axios instance 생성

---
+++ lib/axios.ts
@@ -0,0 +1,41 @@
+"use client";
+
+import axios, { AxiosError, AxiosHeaders } from "axios";
+import { Cookies } from "react-cookie";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+const cookies = new Cookies();
+
+// eslint-disable-next-line import/prefer-default-export
+export const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = cookies.get("token");
+  if (token) {
+    const updatedHeaders = new AxiosHeaders({
+      ...config.headers,
+      Authorization: `Bearer ${token}`,
+    });
+
+    return { ...config, headers: updatedHeaders };
+  }
+
+  return config;
+});
+
+axiosInstance.interceptors.response.use(
+  (res) => res,
+
+  async (error: AxiosError) => {
+    const { config } = error;
+
+    if (config && error.response?.status === 401) {
+      // TODO: 로그인만료 popup 띄우기
+      window.location.href = "/login";
+    }
+
+    return Promise.reject(error);
+  },
+);
