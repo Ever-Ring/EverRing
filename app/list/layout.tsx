101238dde1bac8c5e630d8b735314d08e5dd05f8
오기선 <139519062+gisun55555@users.noreply.github.com>
2025-02-13 17:34:17 +0900

    Revert "🎨 리스트 페이지 퍼블리싱"

--- app/list/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export default function ListLayout({ children }: { children: ReactNode }) {
-  return (
-    <div className="mx-auto min-h-[calc(100vh-3.5rem)] overflow-y-auto bg-gray-50 lg:max-w-[70%]">
-      {children}
-    </div>
-  );
-}
