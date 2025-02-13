514bc97d30470a985613aec34e91e8805d7ed89a
yejiniee <02doyoung01@naver.com>
2025-02-13 09:12:48 +0900

    🚚 move: 폴더 이동

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,5 +1,5 @@
-import AuthPageLayout from "@features/auth/AuthPageLayout";
-import SigninForm from "@features/auth/SigninForm";
+import AuthPageLayout from "@features/auth/components/AuthPageLayout";
+import SigninForm from "@features/auth/components/SigninForm";
 
 export default function Signin() {
   return (
