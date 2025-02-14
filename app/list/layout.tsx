c45d2b2d0ccf87bb8a3948e2a88215abc765fdf0
giseon <pakox123@gmail.com>
2025-02-14 09:15:03 +0900

    🎨 style : 레이아웃 패딩으로일치

--- app/list/layout.tsx
+++ app/list/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function ListLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="mx-auto min-h-[calc(100vh-3.5rem)] overflow-y-auto bg-gray-50 lg:max-w-[70%]">
+    <div className="flex h-full flex-1 overflow-y-auto bg-gray-50 lg:mx-[15%]">
       {children}
     </div>
   );
