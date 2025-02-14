e42d9dbba69c814eb4ecc6002b5246b260a75628
imsoohyeok <skilldoy2@naver.com>
2025-02-14 13:23:02 +0900

    style: 반응형 디자인 추가

--- features/list-detail/FloatingBar.tsx
+++ features/list-detail/FloatingBar.tsx
@@ -4,14 +4,15 @@ import Button from "@components/common/Button";
 function FloatingBar() {
   return (
     <div className="fixed bottom-0 left-0 flex min-h-[64px] w-full justify-center border-t border-gray-900 bg-white">
-      <div className="mx-auto flex w-full max-w-[70%] items-center justify-between px-6 py-5 lg:px-[15%] lg:py-5">
-        <div className="flex flex-col gap-1">
-          <span className="text-lg font-semibold text-gray-900">
+      <div className="mx-auto flex w-full max-w-[70%] items-center justify-between px-4 py-5 md:px-6 lg:px-[15%]">
+        <div className="flex min-w-0 flex-col gap-1">
+          <span className="text-base font-semibold text-gray-900 sm:text-sm">
             더 건강한 나와 팀을 위한 프로그램 🏃‍️️
           </span>
-          <span className="text-sm font-medium text-gray-700">
-            국내 최고 웰니스 전문가와 프로그램을 통해 지친 몸과 마음을
-            회복해봐요
+          <span className="max-w-[250px] text-[12px] font-medium leading-snug text-gray-700 sm:max-w-full sm:whitespace-nowrap">
+            국내 최고 웰니스 전문가와 프로그램을
+            <br className="sm:hidden" />
+            통해 지친 몸과 마음을 회복해봐요
           </span>
         </div>
         <div className="shrink-0">
