ac1babe69b792c463920d053f6ef13546b4a8eb4
yejiniee <02doyoung01@naver.com>
2025-02-14 14:14:41 +0900

    🎨  style: ReviewItem 스타일 수정

--- components/common/ReviewItem.tsx
+++ components/common/ReviewItem.tsx
@@ -28,7 +28,7 @@ export default function ReviewItem({
   isReviewPage,
 }: ReviewItemProps) {
   return (
-    <div className="flex flex-col items-start gap-6 text-gray-700 sm:flex-row">
+    <li className="flex flex-col items-start gap-6 text-gray-700 sm:flex-row">
       {!isReviewPage && (
         <div className="relative h-[9.75rem] w-full sm:w-[17.5rem]">
           <Image
@@ -39,36 +39,37 @@ export default function ReviewItem({
           />
         </div>
       )}
-
-      <div className="flex flex-1 flex-col items-start justify-between gap-2 self-stretch">
-        <div className="flex flex-col items-start gap-[0.625rem] self-stretch">
-          <ReviewRating score={score} />
-          <p className="text-sm font-medium">{comment}</p>
-          {!isReviewPage && (
-            <p className="flex flex-row items-center gap-[0.375rem] text-xs font-medium">
-              <span>{gatheringName} 이용</span>
-              <span>·</span>
-              <span>{gatheringLocation}</span>
-            </p>
-          )}
-        </div>
-        <div className="flex flex-row items-center gap-3">
-          {!isMypage && (
-            <p className="flex flex-row items-center gap-2">
-              <span className="relative size-6">
-                <Image alt="user-profile" fill src={userImage} />
-              </span>
-              <span className="text-xs font-medium">{userName}</span>
-              <span>|</span>
-            </p>
-          )}
-          <span className="text-xs font-medium text-gray-500">
-            {createdAt.toISOString().split("T")[0]}
-          </span>
+      <div className="flex h-[10.5rem] flex-col items-start justify-between self-stretch sm:h-[9.75rem] sm:flex-1">
+        <div className="flex flex-col items-start gap-2 self-stretch">
+          <div className="flex flex-col items-start gap-[0.625rem] self-stretch">
+            <ReviewRating score={score} />
+            <p className="text-sm font-medium">{comment}</p>
+            {!isReviewPage && (
+              <p className="flex flex-row items-center gap-[0.375rem] text-xs font-medium">
+                <span>{gatheringName} 이용</span>
+                <span>·</span>
+                <span>{gatheringLocation}</span>
+              </p>
+            )}
+          </div>
+          <div className="flex flex-row items-center gap-3">
+            {!isMypage && (
+              <p className="flex flex-row items-center gap-2">
+                <span className="relative size-6">
+                  <Image alt="user-profile" fill src={userImage} />
+                </span>
+                <span className="text-xs font-medium">{userName}</span>
+                <span>|</span>
+              </p>
+            )}
+            <span className="text-xs font-medium text-gray-500">
+              {createdAt.toISOString().split("T")[0]}
+            </span>
+          </div>
         </div>
 
         <UnderLine className="w-full shrink-0 self-stretch fill-gray-200" />
       </div>
-    </div>
+    </li>
   );
 }
