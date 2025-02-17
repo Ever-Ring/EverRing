8f31c29db29bfd9560b8ff46fd65b88bf6203168
minzoo <aw12813@gmail.com>
2025-02-17 18:18:07 +0900

    🎨 style: 마이페이지 레이아웃 패딩 값 수정

--- app/mypage/layout.tsx
+++ app/mypage/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function MypageLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex h-full flex-1 flex-col overflow-hidden bg-gray-50 px-6 pt-8 lg:mx-[15%]">
+    <div className="flex h-full flex-1 flex-col overflow-hidden bg-gray-50 px-4 pt-8 sm:px-6 lg:mx-[15%] lg:px-24">
       <main className="h-full">{children}</main>
     </div>
   );
