7996ce3856b0712c55ce52b125f46fb018087fb0
imsoohyeok <skilldoy2@naver.com>
2025-02-17 13:43:49 +0900

    style: h-auto, w-auto 추가

--- components/common/ChipInfo.tsx
+++ components/common/ChipInfo.tsx
@@ -5,7 +5,7 @@ interface ChipInfoProps {
 
 function ChipInfo({ info, variant = "white" }: ChipInfoProps) {
   return (
-    <div className="inline-flex items-center justify-center gap-2.5 rounded bg-gray-900 px-2 py-0.5">
+    <div className="inline-flex h-auto w-auto items-center justify-center gap-2.5 rounded bg-gray-900 px-2 py-0.5">
       <span
         className={`text-sm font-medium leading-5 ${
           variant === "mint" ? "text-mint-600" : "text-white"
