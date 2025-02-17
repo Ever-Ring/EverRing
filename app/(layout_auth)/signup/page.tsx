74ef838e7f4c693e3b1ff40095545e67b059f96a
yejiniee <02doyoung01@naver.com>
2025-02-17 20:24:20 +0900

    🚚 rename: AuthLayout과 혼동되지 않도록 이름 변경

--- app/(layout_auth)/signup/page.tsx
+++ app/(layout_auth)/signup/page.tsx
@@ -1,14 +1,14 @@
-import AuthPageLayout from "@features/auth/components/AuthPageLayout";
+import AuthPageContainer from "@features/auth/components/AuthPageContainer";
 import SignupForm from "@features/auth/components/SignupForm";
 
 export default function Signup() {
   return (
-    <AuthPageLayout
+    <AuthPageContainer
       title="회원가입"
       description="이미 회원이신가요?"
       linkText="로그인"
     >
       <SignupForm />
-    </AuthPageLayout>
+    </AuthPageContainer>
   );
 }
