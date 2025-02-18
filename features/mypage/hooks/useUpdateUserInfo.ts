cfe4d246bf9f0860208d5139ccf98f8ce9091bc5
minzoo <aw12813@gmail.com>
2025-02-18 14:33:06 +0900

    ♻️ refactor: useUpdateUserInfo 커스텀훅 필요없는 코드 삭제

--- features/mypage/hooks/useUpdateUserInfo.ts
+++ features/mypage/hooks/useUpdateUserInfo.ts
@@ -1,9 +1,7 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import MypageApi from "@features/mypage/apis/MypageApi";
 
 export default function useUpdateUserInfo() {
-  const queryClient = useQueryClient();
-
   return useMutation({
     mutationFn: (data: FormData) => MypageApi.updateUserInfo(data),
   });
