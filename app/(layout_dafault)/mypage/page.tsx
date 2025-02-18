6bd172463bbc2290ef484b541badb0d32bcc85f5
minzoo <aw12813@gmail.com>
2025-02-18 10:27:03 +0900

    🎨 style: 마이페이지 스크롤 레이아웃 수정

--- app/(layout_dafault)/mypage/page.tsx
+++ app/(layout_dafault)/mypage/page.tsx
@@ -4,9 +4,11 @@ import MyProfile from "@features/mypage/components/MyProfile";
 
 export default function Mypage() {
   return (
-    <div className="flex h-full flex-col gap-y-7">
-      <span className="text-2xl font-semibold">마이 페이지</span>
-      <div className="flex-1 space-y-7 overflow-y-auto">
+    <div className="flex h-full flex-col">
+      <span className="sticky top-[56px] z-10 bg-gray-50 py-6 text-2xl font-semibold md:top-[60px] md:py-8">
+        마이 페이지
+      </span>
+      <div className="flex flex-1 flex-col gap-y-7">
         <MyProfile />
         {/* <MypageContent /> */}
       </div>
