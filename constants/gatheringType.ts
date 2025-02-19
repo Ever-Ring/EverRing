04ea7050c2b748ba029d14a21cbe5804beb5165f
yejiniee <02doyoung01@naver.com>
2025-02-19 11:06:11 +0900

    ✨ feat: 상수 파일 생성- getheringType 매핑, user 디폴트 프로필 이미지

---
+++ constants/gatheringType.ts
@@ -0,0 +1,8 @@
+const GATHERING_TYPE_MAP: Record<string, string> = {
+  DALLAEMFIT: "나무링",
+  OFFICE_STRETCHING: "에버 스트레칭",
+  MINDFULNESS: "에버 푸드트립",
+  WORKATION: "구름링",
+};
+
+export default GATHERING_TYPE_MAP;
---
+++ constants/user.ts
@@ -0,0 +1,3 @@
+// 나중에 더 추가될 수도 있어서 single export로 해두었습니다.
+// eslint-disable-next-line import/prefer-default-export
+export const DEFAULT_USER_IMAGE = "/image/img-profile-large-default.svg";
