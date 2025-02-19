dfb5a40c9372ce57cfd6375ebb50bc70fca6cf8f
yejiniee <02doyoung01@naver.com>
2025-02-19 11:03:48 +0900

    ♻️ refactor: ReviewItem에 review 객체 넘기도록 수정

--- components/common/ReviewItem.tsx
+++ components/common/ReviewItem.tsx
@@ -3,31 +3,35 @@ import Image from "next/image";
 import UnderLine from "@assets/underline.svg";
 import { ReviewItemProps } from "@customTypes/review";
 import dateFormatter from "@utils/dateFormatter";
+import { DEFAULT_USER_IMAGE } from "@constants/user";
 
-export default function ReviewItem({
-  score,
-  comment,
-  userImage,
-  userName,
-  createdAt,
-}: ReviewItemProps) {
+export default function ReviewItem({ review }: ReviewItemProps) {
+  // TODO 스켈레톤 UI 표시
+  if (!review) {
+    return <div>스켈레톤</div>;
+  }
+
   return (
     <li className="flex flex-1 flex-col items-start gap-4 text-gray-700">
       <div className="flex flex-col items-start gap-2 self-stretch">
         <div className="flex flex-col items-start gap-[0.625rem] self-stretch">
-          <ReviewRating score={score} />
-          <p className="text-sm font-medium">{comment}</p>
+          <ReviewRating score={review?.score} />
+          <p className="text-sm font-medium">{review?.comment}</p>
         </div>
         <div className="flex flex-row items-center gap-3">
           <p className="flex flex-row items-center gap-2">
             <span className="relative size-6">
-              <Image alt="user-profile" fill src={userImage} />
+              <Image
+                alt="user-profile"
+                fill
+                src={review?.User?.image ?? DEFAULT_USER_IMAGE}
+              />
             </span>
-            <span className="text-xs font-medium">{userName}</span>
+            <span className="text-xs font-medium">{review?.User?.name}</span>
             <span>|</span>
           </p>
           <span className="text-xs font-medium text-gray-500">
-            {dateFormatter(createdAt)}
+            {dateFormatter(new Date(review?.createdAt))}
           </span>
         </div>
       </div>
--- components/common/ReviewListWithImage.tsx
+++ components/common/ReviewListWithImage.tsx
@@ -7,20 +7,14 @@ export default function ReviewListwithImage({
 }: ReviewListWithImageProps) {
   return (
     <ol className="flex flex-col items-start gap-6 self-stretch">
-      {reviewData.map((review) => (
-        <ReviewItemWithImage
-          key={review.id}
-          score={review.score}
-          comment={review.comment}
-          userImage={review.User.image}
-          userName={review.User.name}
-          gatheringImage={review.Gathering.image}
-          gatheringName={review.Gathering.name}
-          gatheringLocation={review.Gathering.location}
-          createdAt={review.createdAt}
-          hasUserInfo={hasUserInfo}
-        />
-      ))}
+      {reviewData &&
+        reviewData.map((review) => (
+          <ReviewItemWithImage
+            key={review.id}
+            review={review}
+            hasUserInfo={hasUserInfo}
+          />
+        ))}
     </ol>
   );
 }
