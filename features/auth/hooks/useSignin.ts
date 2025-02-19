da89a4e762649fe705811de5d414bba9b6a3b1db
yejiniee <02doyoung01@naver.com>
2025-02-19 11:07:41 +0900

    🐛 fix: AuthApi import 경로 수정

--- features/auth/hooks/useSignin.ts
+++ features/auth/hooks/useSignin.ts
@@ -1,7 +1,7 @@
 "use client";
 
 import { useMutation } from "@tanstack/react-query";
-import AuthApi from "@features/auth/apis/AuthApi";
+import AuthApi from "@apis/AuthApi";
 
 export default function useSignin() {
   return useMutation({
