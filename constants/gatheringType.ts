36acc56bbd47485a45ddc3bd223a973b3dadecf7
yejiniee <02doyoung01@naver.com>
2025-02-20 17:30:34 +0900

    ✨ feat: gathering type 파일 추가

--- constants/gatheringType.ts
+++ constants/gatheringType.ts
@@ -1,8 +1,15 @@
-const GATHERING_TYPE_MAP: Record<string, string> = {
+export const GATHERING_TYPE = {
+  DALLAEMFIT: "DALLAEMFIT",
+  OFFICE_STRETCHING: "OFFICE_STRETCHING",
+  MINDFULNESS: "MINDFULNESS",
+  WORKATION: "WORKATION",
+} as const;
+
+export type GatheringType = keyof typeof GATHERING_TYPE;
+
+export const GATHERING_TYPE_MAP: Record<GatheringType, string> = {
   DALLAEMFIT: "나무링",
   OFFICE_STRETCHING: "에버 스트레칭",
   MINDFULNESS: "에버 푸드트립",
   WORKATION: "구름링",
 };
-
-export default GATHERING_TYPE_MAP;
