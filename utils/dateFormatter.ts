797aa2b692a7d71303b90271b5785a3d963e27e1
giseon <pakox123@gmail.com>
2025-02-21 11:29:11 +0900

    ✨ feat : formatDateTime 임시 유틸 추가

--- utils/dateFormatter.ts
+++ utils/dateFormatter.ts
@@ -24,3 +24,21 @@ export function formatDateTime(dateTimeString: string): string {
 
   return `${formattedDate} ・ ${formattedTime}`;
 }
+
+// formatDateTime 임시 유틸 사용 수정 후 삭제예정
+export function formatDateTime2(dateTimeString: string) {
+  const dateObj = new Date(dateTimeString);
+
+  const date = new Intl.DateTimeFormat("ko-KR", {
+    month: "long",
+    day: "numeric",
+  }).format(dateObj);
+
+  const time = dateObj.toLocaleTimeString("ko-KR", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  });
+
+  return { date, time };
+}
