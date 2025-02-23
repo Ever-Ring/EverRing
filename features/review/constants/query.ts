46973782935f963c576e8c843548be4245e2f1cd
yejiniee <02doyoung01@naver.com>
2025-02-23 15:01:18 +0900

    🐛 fix: 필터링 제대로 안 먹는 오류 수정

---
+++ features/review/constants/query.ts
@@ -0,0 +1,13 @@
+/* eslint-disable import/prefer-default-export */
+import { DESC } from "@features/review/constants/review";
+import { GATHERING_TYPE } from "@constants/gatheringType";
+
+export const INITIAL_PARAMS = {
+  type: GATHERING_TYPE.DALLAEMFIT,
+  location: undefined,
+  date: undefined,
+  sortBy: undefined,
+  sortOrder: DESC,
+  limit: 2,
+  offset: 0,
+};
--- types/reviewApi.d.ts
+++ types/reviewApi.d.ts
@@ -5,20 +5,14 @@ export interface CommonReviewParams {
 
 export interface ReviewQueryParams extends CommonReviewParams {
   userId?: number;
+  type: GatheringType;
   location?: string;
-  // location?: "건대입구" | "을지로3가" | "신림" | "홍대입구" | "지역전체";
   date?: string;
   registrationEnd?: string;
-  // sortBy?: "createdAt" | "score" | "participantCount" | undefined;
   sortBy?: string;
-
   sortOrder?: "asc" | "desc";
   limit?: number;
   offset?: number;
 }
-
-// export interface Location {
-//   location?: "건대입구" | "을지로3가" | "신림" | "홍대입구" | "지역전체";
-// }
 
 export interface ReviewScoreQueryParams extends CommonReviewParams {}
