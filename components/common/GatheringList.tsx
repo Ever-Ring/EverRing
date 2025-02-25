075c852cfd1039d58e1350611bf3594e8fc5cfd7
giseon <pakox123@gmail.com>
2025-02-25 16:30:30 +0900

    feat : 빈 모임 리스트 처리

--- components/common/GatheringList.tsx
+++ components/common/GatheringList.tsx
@@ -1,16 +1,31 @@
 import GatheringItem from "@components/common/GatheringItem";
-import { GatheringListProps } from "@customTypes/gathering";
+import { Gathering, GatheringListProps } from "@customTypes/gathering";
 
 export default function GatheringList({ gatherings }: GatheringListProps) {
+  console.log(gatherings, "📡 gatherings props");
+
+  if (!Array.isArray(gatherings) || gatherings.length === 0) {
+    return (
+      <div className="flex flex-col items-center justify-center">
+        <p>아직 모임이 없어요.</p>
+        <p>지금 바로 모임을 만들어보세요</p>
+      </div>
+    );
+  }
+
+  const filteredGatherings: Gathering[] = gatherings.filter(
+    (gatheringItem) => gatheringItem.canceledAt === null,
+  );
+
+  if (filteredGatherings.length === 0) {
+    return null;
+  }
+
   return (
     <ol className="flex flex-col items-start gap-6 self-stretch">
-      {gatherings.map(
-        (gatheringItem) =>
-          gatheringItem.canceledAt &&
-          gatheringItem.createdBy !== null && (
-            <GatheringItem key={gatheringItem.id} gathering={gatheringItem} />
-          ),
-      )}
+      {filteredGatherings.map((gatheringItem) => (
+        <GatheringItem key={gatheringItem.id} gathering={gatheringItem} />
+      ))}
     </ol>
   );
 }
