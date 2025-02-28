13453bbc66ff1213e5f6f8801009e09ceab1e0d5
imsoohyeok <skilldoy2@naver.com>
2025-02-28 14:47:44 +0900

    ✨ feat: / -> /list redirect 추가

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,24 @@
 import { NextResponse, type NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
   const token = request.cookies.get("token");
 
-  if (!token) {
-    return NextResponse.redirect(new URL("/signin", request.url));
+  if (pathname === "/") {
+    const url = request.nextUrl.clone();
+    url.pathname = "/list";
+    return NextResponse.redirect(url);
+  }
+
+  if (pathname.startsWith("/mypage")) {
+    if (!token) {
+      return NextResponse.redirect(new URL("/signin", request.url));
+    }
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/mypage/:path*"],
+  matcher: ["/mypage/:path*", "/"],
 };
