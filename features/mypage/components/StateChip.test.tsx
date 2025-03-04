2cc1eaad552014635050111b77e3893775903360
minzoo <aw12813@gmail.com>
2025-03-04 22:02:20 +0900

    🧪 test: StateChip 테스트코드 수정

--- features/mypage/components/StateChip.test.tsx
+++ features/mypage/components/StateChip.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import StateChip from "./StateChip";
+import StateChip from "@features/mypage/components/StateChip";
 
 describe("StateChip Component Test", () => {
   test("should render '이용 예정' text by default", () => {
@@ -13,12 +13,12 @@ describe("StateChip Component Test", () => {
   });
 
   test("should render '이용 완료' text when isCompleted is true", () => {
-    render(<StateChip isCompleted={true} />);
+    render(<StateChip isCompleted />);
     expect(screen.getByText("이용 완료").textContent).toBe("이용 완료");
   });
 
   test("should render '개설 대기' text when isPending is true", () => {
-    render(<StateChip isCompleted={false} isPending={true} />);
+    render(<StateChip isCompleted={false} isPending />);
     expect(screen.getByText("개설 대기").textContent).toBe("개설 대기");
   });
 
