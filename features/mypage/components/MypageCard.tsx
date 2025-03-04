f9505248f31b80dd09423e993ccc0160d5488e7d
minzoo <aw12813@gmail.com>
2025-03-04 10:07:13 +0900

    ♻️ refactor: MypageCard props 수정

--- features/mypage/components/MypageCard.tsx
+++ features/mypage/components/MypageCard.tsx
@@ -7,49 +7,38 @@ import useModalStore from "@stores/modalStore";
 import WriteReviewModal from "@features/mypage/components/WriteReviewModal";
 import { useState } from "react";
 import { MIN_PARTICIPANTS } from "@constants/gathering";
+import { GatheringJoined } from "@customTypes/gathering";
 
 interface MypageCardProps {
-  gatheringId: number;
-  name: string;
-  location: string;
-  dateTime: string;
-  image: string;
-  isCompleted?: boolean;
-  participantCount: number;
-  capacity: number;
+  getheringData: GatheringJoined;
   isMyGatheringTab?: boolean;
   isMadeByMe?: boolean;
 }
 
 export default function MypageCard({
-  gatheringId,
-  name,
-  location,
-  dateTime,
-  image,
-  isCompleted,
+  getheringData,
   isMyGatheringTab,
   isMadeByMe,
-  participantCount,
-  capacity,
 }: MypageCardProps) {
-  const formattedDateTime = formatDateTime(dateTime);
+  const formattedDateTime = formatDateTime(getheringData.dateTime ?? "");
   const { mutate: deleteGatheringJoined } = useDeleteGatheringJoined();
   const { openModal } = useModalStore();
   const [isWriteReviewModalOpen, setIsWriteReviewModalOpen] = useState(false);
   const closeWriteReviewModal = () => {
     setIsWriteReviewModalOpen(false);
   };
+  const isRegistrationEnded =
+    new Date(getheringData.registrationEnd) < new Date();
 
   function handleClick() {
-    if (isCompleted) {
+    if (getheringData.isCompleted) {
       setIsWriteReviewModalOpen(true);
     } else {
       openModal({
         text: "정말 모임 참여를 취소하시겠습니까?",
         hasTwoButton: true,
         onConfirm: () => {
-          deleteGatheringJoined(gatheringId);
+          deleteGatheringJoined(getheringData.id);
           // TODO 취소 후 알림 표시
           alert("모임 참여가 취소되었습니다.");
         },
@@ -61,7 +50,7 @@ export default function MypageCard({
     <div className="mb-6 flex flex-col gap-4 border-b-2 border-dotted pb-6 md:flex-row">
       <div className="relative h-[9.75rem] w-full rounded-3xl md:w-[17.5rem]">
         <Image
-          src={image}
+          src={getheringData.image}
           alt="gathering image"
           fill
           sizes="100%"
@@ -71,15 +60,15 @@ export default function MypageCard({
       <div className="relative flex w-full flex-1 flex-col gap-y-3">
         {isMyGatheringTab && (
           <StateChip
-            isPending={participantCount < MIN_PARTICIPANTS}
-            isCompleted={isCompleted}
+            isPending={getheringData.participantCount < MIN_PARTICIPANTS}
+            isCompleted={getheringData.isCompleted}
           />
         )}
         <div className="flex flex-col gap-y-1">
           <div className="flex flex-row gap-x-2">
-            <p className="text-lg font-semibold">{name}</p>
+            <p className="text-lg font-semibold">{getheringData.name}</p>
             <p className="text-lg font-semibold">|</p>
-            <p>{location}</p>
+            <p>{getheringData.location}</p>
           </div>
           <div className="flex flex-row items-center">
             <p className="mr-3">{formattedDateTime}</p>
@@ -91,17 +80,19 @@ export default function MypageCard({
               className="mr-1"
             />
             <p>
-              {participantCount}/{capacity}
+              {getheringData.participantCount}/{getheringData.capacity}
             </p>
           </div>
         </div>
         {!isMadeByMe && (
           <div className="md:absolute md:bottom-0 md:left-0">
             <Button
-              text={isCompleted ? "리뷰 작성하기" : "예약 취소하기"}
+              text={
+                getheringData.isCompleted ? "리뷰 작성하기" : "예약 취소하기"
+              }
               size="small"
-              disabled={false}
-              variant={isCompleted ? "solid" : "outlined"}
+              disabled={isRegistrationEnded && !getheringData.isCompleted}
+              variant={getheringData.isCompleted ? "solid" : "outlined"}
               type="button"
               onClick={() => {
                 handleClick();
@@ -114,7 +105,7 @@ export default function MypageCard({
         <WriteReviewModal
           isOpen={isWriteReviewModalOpen}
           onClose={closeWriteReviewModal}
-          gatheringId={gatheringId}
+          gatheringId={getheringData.id}
         />
       )}
     </div>
