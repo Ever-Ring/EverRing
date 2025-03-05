2b2b3ce2646a5539ad51fb8edb1dbf9b4f4d1b62
minzoo <aw12813@gmail.com>
2025-03-05 18:28:59 +0900

    🎨 style: 마이페이지 sticky 영역 z-index 수정

--- app/(layout_dafault)/mypage/page.tsx
+++ app/(layout_dafault)/mypage/page.tsx
@@ -4,7 +4,7 @@ import MypageContent from "@features/mypage/components/MypageContent";
 export default function Mypage() {
   return (
     <div className="flex h-full flex-col">
-      <span className="sticky top-[56px] z-50 bg-gray-50 py-6 text-2xl font-semibold md:top-[60px] md:py-8">
+      <span className="sticky top-[56px] z-20 bg-gray-50 py-6 text-2xl font-semibold md:top-[60px] md:py-8">
         마이 페이지
       </span>
       <div className="flex flex-1 flex-col gap-y-7">
