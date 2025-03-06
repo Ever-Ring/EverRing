b04c40866b870721a556185b59703d25983bdd63
yejiniee <02doyoung01@naver.com>
2025-03-06 15:40:31 +0900

    ♻️ refactor: 리뷰페이지 SSR로 prefetch후 리액트쿼리로 무한스크롤되도록 수정

--- features/review/hooks/useGetReviewData.ts
+++ features/review/hooks/useGetReviewData.ts
@@ -1,67 +1,13 @@
 "use client";
 
 import { useInfiniteQuery } from "@tanstack/react-query";
-import ReviewApi from "@apis/ReviewApi";
 import { ReviewQueryParams } from "@customTypes/reviewApi";
-import { Review } from "@customTypes/review";
-import { INITIAL_PARAMS } from "@features/review/constants/query";
+import { getClientReviewQuery } from "@features/review/hooks/useReviewQuery";
 
-export default function useGetReviewData(
-  filter: ReviewQueryParams,
-  {
-    initialData = [],
-    totalItemCount,
-  }: {
-    initialData: Review[];
-    totalItemCount: number;
-  },
-) {
-  const isInitialParams =
-    JSON.stringify(filter) === JSON.stringify(INITIAL_PARAMS);
-
-  const {
-    data,
-    isError,
-    fetchNextPage,
-    hasNextPage,
-    isFetching,
-    isFetchingNextPage,
-  } = useInfiniteQuery({
-    queryKey: ["reviews", filter],
-    queryFn: async ({ pageParam = filter.offset }) => {
-      const response = await ReviewApi.getReviewData({
-        ...filter,
-        offset: pageParam,
-      });
-
-      return response.data;
-    },
-    getNextPageParam: (lastPage, allPages) => {
-      const loadedItems = allPages.flatMap((page) => page.data).length;
-      return loadedItems < lastPage.totalItemCount ? loadedItems : undefined;
-    },
-    initialData: isInitialParams
-      ? {
-          pages: [
-            {
-              data: initialData,
-              totalItemCount,
-            },
-          ],
-          pageParams: [0],
-        }
-      : undefined,
-    initialPageParam: 0,
-  });
-
-  const flatData = data?.pages.flatMap((page) => page.data) || [];
-
+export default function useGetReviewData(filter: ReviewQueryParams) {
+  const query = useInfiniteQuery(getClientReviewQuery(filter));
   return {
-    data: flatData,
-    isError,
-    fetchNextPage,
-    hasNextPage,
-    isFetching,
-    isFetchingNextPage,
+    ...query,
+    data: query.data?.pages.flatMap((page) => page.data) ?? [],
   };
 }
