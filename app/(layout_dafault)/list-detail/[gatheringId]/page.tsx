39d97ea030ca87c264d7e5b43b22736d64982142
imsoohyeok <skilldoy2@naver.com>
2025-03-10 14:25:16 +0900

    fix: page.tsx 파일 간단화

--- app/(layout_dafault)/list-detail/[gatheringId]/page.tsx
+++ app/(layout_dafault)/list-detail/[gatheringId]/page.tsx
@@ -1,36 +1,27 @@
+import { notFound } from "next/navigation";
+import { ReactElement } from "react";
+import { axiosInstance } from "@lib/axios";
 import { Gathering } from "@customTypes/gathering";
 import ListDetailContent from "@features/list-detail/components/ListDetailContent";
-import { axiosInstance } from "@lib/axios";
-import { notFound } from "next/navigation";
-import { JSX } from "react";
 
 export default async function ListDetailPage({
   params,
 }: {
   params: Promise<{ gatheringId: string }>;
-}): Promise<JSX.Element> {
+}): Promise<ReactElement> {
   const { gatheringId } = await params;
 
   if (!gatheringId) {
     notFound();
   }
 
-  try {
-    const response = await axiosInstance.get(`/gatherings/${gatheringId}`);
-    const gatheringData: Gathering = response.data;
+  const response = await axiosInstance.get(`/gatherings/${gatheringId}`);
+  const gatheringData: Gathering = response.data;
 
-    if (!gatheringData) {
-      notFound();
-    }
-
-    return (
-      <ListDetailContent
-        gatheringId={Number(gatheringId)}
-        gathering={gatheringData}
-      />
-    );
-  } catch (error) {
-    console.error("API fetch error:", error);
-    notFound();
-  }
+  return (
+    <ListDetailContent
+      gatheringId={Number(gatheringId)}
+      gathering={gatheringData}
+    />
+  );
 }
