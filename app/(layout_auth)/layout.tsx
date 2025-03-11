bbe62068c7e3142704e07cf7b79793d978426d2d
yejiniee <02doyoung01@naver.com>
2025-03-11 16:30:19 +0900

    ♻️ refactor: 랜딩페이지 생성 위해 레이아웃 수정

--- app/(layout_auth)/layout.tsx
+++ app/(layout_auth)/layout.tsx
@@ -2,8 +2,10 @@ import { ReactNode } from "react";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex w-full justify-center px-4 md:px-[4.25rem] lg:px-0 lg:py-14">
-      {children}
+    <div className="flex w-full justify-center bg-gray-100 lg:px-[15%]">
+      <div className="flex w-full justify-center px-4 md:px-[4.25rem] lg:px-0 lg:py-14">
+        {children}
+      </div>
     </div>
   );
 }
