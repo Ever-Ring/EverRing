e93f03e8bf1ab3d2af659c7dda9d69d183551078
yejiniee <02doyoung01@naver.com>
2025-03-12 13:28:15 +0900

    🚧 WIP: 랜딩페이지 생성

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,35 @@
-export default function Home() {
-  return <div>initial commit</div>;
+"use client";
+
+import AnimatedSection from "@features/landing/components/AnimatedSection";
+import LandingHero from "@features/landing/components/LandingHeroSection";
+import FeatureSection from "@features/landing/components/FeatureSection";
+import ReviewSection from "@features/landing/components/ReviewSection";
+import ServiceShowcaseSection from "@features/landing/components/ServiceShowcaseSection";
+import FooterSection from "@features/landing/components/FooterSection";
+
+export default function Landing() {
+  return (
+    <div className="relative w-full bg-gray-50">
+      <AnimatedSection>
+        <LandingHero />
+      </AnimatedSection>
+
+      <AnimatedSection>
+        <FeatureSection />
+      </AnimatedSection>
+      {/* <AnimatedSection> relative top-[60px] flex-1 */}
+
+      <div className="">
+        <ServiceShowcaseSection />
+      </div>
+
+      <AnimatedSection>
+        <ReviewSection />
+      </AnimatedSection>
+
+      <AnimatedSection>
+        <FooterSection />
+      </AnimatedSection>
+    </div>
+  );
 }
