dbda802350f5edb13ef1a8798de544d4703a88b8
giseon <pakox123@gmail.com>
2025-03-13 15:06:59 +0900

    ♻️ refactor : 모임리스트 SSR 적용 범위 늘리기

--- app/(layout_dafault)/list/page.tsx
+++ app/(layout_dafault)/list/page.tsx
@@ -3,6 +3,7 @@ import ListContent from "@features/list/components/ListContent";
 import { HydrationBoundary } from "@tanstack/react-query";
 import { getServerInfiniteGatheringsQuery } from "@features/list/utills/gatheringsQuery";
 import { getDehydratedQuery } from "@lib/reactQueryUtils";
+import HeartImage from "@assets/img-head-class.svg";
 
 export default async function List() {
   const { queryKey, queryFn } = getServerInfiniteGatheringsQuery();
@@ -10,6 +11,15 @@ export default async function List() {
   return (
     <HydrationBoundary state={state}>
       <Suspense fallback={<div>로딩 중...</div>}>
+        <section className="mb-6 flex items-center justify-start gap-4 sm:mb-8">
+          <HeartImage className="w-18 h-18" />
+          <div>
+            <p className="text-sm font-medium">함께 할 사람이 없나요?</p>
+            <p className="mt-1 text-2xl font-semibold">
+              지금 모임에 참여해보세요
+            </p>
+          </div>
+        </section>
         <ListContent />
       </Suspense>
     </HydrationBoundary>
