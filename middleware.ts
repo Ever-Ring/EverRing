37d99dd41c8d7db6c0e2b4da9fde9766dd29011d
minzoo <aw12813@gmail.com>
2025-03-13 16:22:56 +0900

    ✨ feat: auth 페이지 접근 제한 미들웨어 수정
    로그인한 사용자의 경우 signin, signup, 랜딩페이지 접근 못하게 수정

--- middleware.ts
+++ middleware.ts
@@ -4,21 +4,25 @@ export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const token = request.cookies.get("token");
 
-  if (pathname === "/") {
+  if (pathname === "/" && token) {
     const url = request.nextUrl.clone();
     url.pathname = "/list";
     return NextResponse.redirect(url);
   }
+  if (pathname.startsWith("/signin") && token) {
+    return NextResponse.redirect(new URL("/list", request.url));
+  }
+  if (pathname.startsWith("/signup") && token) {
+    return NextResponse.redirect(new URL("/list", request.url));
+  }
 
-  if (pathname.startsWith("/mypage")) {
-    if (!token) {
-      return NextResponse.redirect(new URL("/signin", request.url));
-    }
+  if (pathname.startsWith("/mypage") && !token) {
+    return NextResponse.redirect(new URL("/signin", request.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/mypage/:path*", "/"],
+  matcher: ["/mypage/:path*", "/signin/:path*", "/signup/:path*", "/"],
 };
