fa420da2472c7a41ad40c28abb77e745bef528a9
yejiniee <02doyoung01@naver.com>
2025-03-14 09:50:04 +0900

    ✨ feat: FOUT에서 FOIT 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import Head from "next/head";
 import "./globals.css";
 import Gnb from "@components/common/Gnb";
 import ReactQueryProvider from "@providers/ReactQueryProvider";
@@ -14,7 +13,7 @@ export const metadata: Metadata = {
 
 const pretendard = localFont({
   src: "../public/fonts/PretendardVariable.woff2",
-  display: "swap",
+  display: "block",
   variable: "--font-pretendard",
 });
 
@@ -24,30 +23,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
-      <Head>
-        <link
-          rel="preload"
-          href="/fonts/PretendardVariable.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-      </Head>
-      <html lang="ko">
-        <body
-          className={`flex h-screen flex-col bg-white ${pretendard.variable} font-pretendard antialiased`}
-        >
-          <ReactQueryProvider>
-            <div className="fixed left-0 top-0 z-20 w-full">
-              <Gnb />
-            </div>
-            <div className="flex w-full flex-1 pt-[54px] md:pt-[60px]">
-              {children}
-            </div>
-          </ReactQueryProvider>
-        </body>
-      </html>
-    </>
+    <html lang="ko">
+      <body
+        className={`flex h-screen flex-col bg-white ${pretendard.variable} font-pretendard antialiased`}
+      >
+        <ReactQueryProvider>
+          <div className="fixed left-0 top-0 z-20 w-full">
+            <Gnb />
+          </div>
+          <div className="flex w-full flex-1 pt-[54px] md:pt-[60px]">
+            {children}
+          </div>
+        </ReactQueryProvider>
+      </body>
+    </html>
   );
 }
