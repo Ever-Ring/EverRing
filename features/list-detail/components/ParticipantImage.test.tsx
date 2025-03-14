ca97b39c5f7e3b7d605f4987b468bc62c9af5fba
imsoohyeok <skilldoy2@naver.com>
2025-03-15 01:27:03 +0900

    test: ParticipantImage 테스트 코드 수정

--- features/list-detail/components/ParticipantImage.test.tsx
+++ features/list-detail/components/ParticipantImage.test.tsx
@@ -5,20 +5,22 @@ import ParticipantImage from "@features/list-detail/components/ParticipantImage"
 describe("ParticipantImage Component", () => {
   it("should render a div with the background image when imageUrl is provided", () => {
     const imageUrl = "test-image.png";
-    const { container } = render(<ParticipantImage imageUrl={imageUrl} />);
+    const { container } = render(
+      <ParticipantImage imageUrl={imageUrl} alt="" />,
+    );
     const divElement = container.firstChild as HTMLElement;
     expect(divElement).toHaveStyle(`background-image: url("${imageUrl}")`);
   });
 
   it("should render a div with no background image when imageUrl is not provided", () => {
-    const { container } = render(<ParticipantImage />);
+    const { container } = render(<ParticipantImage alt="" />);
     const divElement = container.firstChild as HTMLElement;
     expect(divElement).toHaveStyle("background-image: none");
   });
 
   it("should have the correct classes applied", () => {
     const { container } = render(
-      <ParticipantImage imageUrl="test-image.png" />,
+      <ParticipantImage imageUrl="test-image.png" alt="" />,
     );
     const divElement = container.firstChild as HTMLElement;
     expect(divElement).toHaveClass(
