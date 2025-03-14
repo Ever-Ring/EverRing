15b0fe8e6959909a668509dd8065b235eaa7c35f
giseon <pakox123@gmail.com>
2025-03-14 17:25:08 +0900

    ✨ feat : 리스트컨테이너 마감체크박스 적용

--- features/list/components/ListContainer.tsx
+++ features/list/components/ListContainer.tsx
@@ -7,9 +7,12 @@ import { GatheringViewModel } from "@features/list/hooks/GatheringViewModel";
 import CreateGatheringButton from "@features/list/components/CreateGatheringButton";
 import ChipFilterGroup from "@components/common/ChipFilterGroup";
 import FilterOptions from "@features/list/components/FilterOptions";
+import ExpiredFilterCheckbox from "@components/common/ExpiredFilterCheckbox";
 
 export default function ListContainer() {
   const {
+    showExpired,
+    setShowExpired,
     selectedTabIndex,
     setTabIndex,
     typeFilter,
@@ -24,6 +27,8 @@ export default function ListContainer() {
     isFetchingNextPage,
     loadMoreRef,
   } = GatheringViewModel();
+  console.log(filters);
+  console.log(sortBy);
 
   return (
     <div className="flex w-full flex-col">
@@ -36,14 +41,19 @@ export default function ListContainer() {
         />
         <CreateGatheringButton />
       </section>
-      <section className="flex justify-start gap-2">
+      <section className="flex justify-between">
         <ChipFilterGroup
           subChips={subChips}
           typeFilter={typeFilter}
           setTypeFilter={setTypeFilter}
         />
+        <ExpiredFilterCheckbox
+          showExpired={showExpired}
+          setShowExpired={setShowExpired}
+        />
       </section>
       <hr className="my-4 w-full border-t-2 border-gray-200" />
+
       <section className="mb-4 sm:mb-6">
         <FilterOptions
           selectedTabIndex={selectedTabIndex}
@@ -55,7 +65,7 @@ export default function ListContainer() {
         />
       </section>
       <section>
-        <GatheringList gatherings={gatherings} />
+        <GatheringList gatherings={gatherings} showExpired={showExpired} />
       </section>
       <div ref={loadMoreRef} className="bg-red-500 w-30 z-10 h-10 text-center">
         {isFetchingNextPage ? "로딩 중..." : ""}
