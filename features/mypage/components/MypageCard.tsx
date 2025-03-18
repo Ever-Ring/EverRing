6c692761d174192264ecf9022092b2f0cfa6b389
minzoo <aw12813@gmail.com>
2025-03-18 09:23:40 +0900

    ♻️ refactor: MypageCard 컴포넌트 구조 수정
    카드 전체에 Link 적용 및 모달 구조 개선

--- features/mypage/components/MypageCard.tsx
+++ features/mypage/components/MypageCard.tsx
@@ -5,11 +5,12 @@ import { formatDateTime } from "@utils/dateFormatter";
 import { useDeleteGatheringJoined } from "@features/mypage/hooks/useDeleteGatheringJoinded";
 import useModalStore from "@stores/modalStore";
 import WriteReviewModal from "@features/mypage/components/WriteReviewModal";
-import { useState } from "react";
+import { useState, MouseEvent } from "react";
 import { MIN_PARTICIPANTS } from "@constants/gathering";
 import { GatheringJoined } from "@customTypes/gathering";
 import GatheringClosureNotice from "@components/common/GatheringClosureNotice";
 import Link from "next/link";
+import ModalPortal from "@components/common/ModalPortal";
 
 interface MypageCardProps {
   gatheringData: GatheringJoined;
@@ -26,15 +27,23 @@ export default function MypageCard({
     gatheringData.dateTime ?? "",
   );
   const { mutate: deleteGatheringJoined } = useDeleteGatheringJoined();
-  const { openModal } = useModalStore();
+  const { openModal, closeModal } = useModalStore();
   const [isWriteReviewModalOpen, setIsWriteReviewModalOpen] = useState(false);
   const closeWriteReviewModal = () => {
     setIsWriteReviewModalOpen(false);
   };
   const isRegistrationEnded =
     new Date(gatheringData.registrationEnd) < new Date();
+  const buttonText = gatheringData.isCompleted
+    ? "리뷰 작성하기"
+    : "참여 취소하기";
 
-  function handleClick() {
+  const buttonVariant = gatheringData.isCompleted ? "solid" : "outlined";
+  const isButtonDisabled = isRegistrationEnded && !gatheringData.isCompleted;
+
+  function handleClick(e: MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+
     if (gatheringData.isCompleted) {
       setIsWriteReviewModalOpen(true);
     } else {
@@ -42,85 +51,94 @@ export default function MypageCard({
         text: "정말 모임 참여를 취소하시겠습니까?",
         hasTwoButton: true,
         onConfirm: () => {
-          deleteGatheringJoined(gatheringData.id);
-          // TODO 취소 후 알림 표시
-          alert("모임 참여가 취소되었습니다.");
+          deleteGatheringJoined(gatheringData.id, {
+            onSuccess: () => {
+              closeModal();
+              openModal({
+                text: "참여 취소되었습니다!",
+                hasTwoButton: false,
+              });
+            },
+          });
         },
       });
     }
   }
 
   return (
-    <div className="relative mb-6 flex flex-col gap-4 border-b-2 border-dotted pb-6 md:flex-row">
-      <div className="relative h-[9.75rem] w-full rounded-3xl md:w-[17.5rem]">
-        <Image
-          src={gatheringData.image}
-          alt="gathering image"
-          fill
-          sizes="100%"
-          className="h-full w-full rounded-3xl object-cover"
-        />
-      </div>
-      <div className="relative flex w-full flex-1 flex-col gap-y-3">
-        {isMyGatheringTab && (
-          <StateChip
-            isPending={gatheringData.participantCount < MIN_PARTICIPANTS}
-            isCompleted={gatheringData.isCompleted}
-          />
-        )}
-        <div className="flex flex-col gap-y-1">
-          <Link href={`/list-detail/${gatheringData.id}`}>
-            <div className="flex flex-row gap-x-2">
-              <p className="text-lg font-semibold">{gatheringData.name}</p>
-              <p className="text-lg font-semibold">|</p>
-              <p>
-                {gatheringData.type === "WORKATION"
-                  ? "온라인"
-                  : gatheringData.location}
-              </p>
-              <span className="pl-1 font-semibold text-gray-500"> 〉 </span>
-            </div>
-          </Link>
-          <div className="flex flex-row items-center">
-            <p className="mr-3">
-              {formattedDate} ・ {formattedTime}
-            </p>
+    <div className="relative mb-6 flex flex-col gap-4 border-b-2 border-dotted pb-6">
+      <Link href={`/list-detail/${gatheringData.id}`} passHref>
+        <div className="flex flex-col gap-4 md:flex-row">
+          <div className="relative h-[9.75rem] w-full rounded-3xl md:w-[17.5rem]">
             <Image
-              src="/image/person.svg"
-              alt="participant icon"
-              width={16}
-              height={16}
-              className="mr-1"
+              src={gatheringData.image}
+              alt="gathering image"
+              fill
+              sizes="100%"
+              className="h-full w-full rounded-3xl object-cover"
             />
-            <p>
-              {gatheringData.participantCount}/{gatheringData.capacity}
-            </p>
+          </div>
+          <div className="relative flex w-full flex-1 flex-col gap-y-3">
+            {isMyGatheringTab && (
+              <StateChip
+                isPending={gatheringData.participantCount < MIN_PARTICIPANTS}
+                isCompleted={gatheringData.isCompleted}
+              />
+            )}
+            <div className="flex flex-col gap-y-1">
+              <div className="flex flex-row gap-x-2">
+                <p className="text-lg font-semibold">{gatheringData.name}</p>
+                <p className="text-lg font-semibold">|</p>
+                <p>
+                  {gatheringData.type === "WORKATION"
+                    ? "온라인"
+                    : gatheringData.location}
+                </p>
+                <span className="pl-1 font-semibold text-gray-500"> 〉 </span>
+              </div>
+              <div className="flex flex-row items-center">
+                <p className="mr-3">
+                  {formattedDate} ・ {formattedTime}
+                </p>
+                <Image
+                  src="/image/person.svg"
+                  alt="participant icon"
+                  width={16}
+                  height={16}
+                  className="mr-1"
+                />
+                <p>
+                  {gatheringData.participantCount}/{gatheringData.capacity}
+                </p>
+              </div>
+            </div>
+            <div className="flex w-full items-center justify-start">
+              {!isMadeByMe && (
+                <Button
+                  text={buttonText}
+                  size="small"
+                  disabled={isButtonDisabled}
+                  variant={buttonVariant}
+                  type="button"
+                  onClick={(e) => {
+                    handleClick(e);
+                  }}
+                />
+              )}
+            </div>
           </div>
         </div>
-        <div className="bottom-0 flex w-full items-center justify-between md:absolute">
-          {!isMadeByMe && (
-            <Button
-              text={
-                gatheringData.isCompleted ? "리뷰 작성하기" : "참여 취소하기"
-              }
-              size="small"
-              disabled={isRegistrationEnded && !gatheringData.isCompleted}
-              variant={gatheringData.isCompleted ? "solid" : "outlined"}
-              type="button"
-              onClick={() => {
-                handleClick();
-              }}
-            />
-          )}
-        </div>
-      </div>
+      </Link>
+
       <GatheringClosureNotice isCanceled={!!gatheringData.canceledAt} />
       {isWriteReviewModalOpen && (
-        <WriteReviewModal
-          isOpen={isWriteReviewModalOpen}
-          onClose={closeWriteReviewModal}
-          gatheringId={gatheringData.id}
-        />
+        <ModalPortal>
+          <WriteReviewModal
+            isOpen={isWriteReviewModalOpen}
+            onClose={closeWriteReviewModal}
+            gatheringId={gatheringData.id}
+          />
+        </ModalPortal>
       )}
     </div>
   );
